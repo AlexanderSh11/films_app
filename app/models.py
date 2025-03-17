cea8c5671904c0495c98026b13d6f5dd2045c85b
Александр Шаринский <alexandersh@tpu.ru>
2025-03-17 23:59:46 +0700

    Оформление главной страницы. Шаблон html для каталога фильмов (с данными из БД)

--- app/models.py
+++ app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Movies(models.Model):
+    name = models.TextField(primary_key=True)  # The composite primary key (name, year) found, that is not supported. The first column is selected.
+    rating = models.TextField(blank=True, null=True)
+    genre = models.TextField(blank=True, null=True)
+    year = models.IntegerField()
+    released = models.TextField(blank=True, null=True)
+    score = models.FloatField(blank=True, null=True)
+    votes = models.FloatField(blank=True, null=True)
+    director = models.TextField(blank=True, null=True)
+    writer = models.TextField(blank=True, null=True)
+    star = models.TextField(blank=True, null=True)
+    country = models.TextField(blank=True, null=True)
+    budget = models.FloatField(blank=True, null=True)
+    gross = models.FloatField(blank=True, null=True)
+    company = models.TextField(blank=True, null=True)
+    runtime = models.FloatField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'movies'
+        unique_together = (('name', 'year'),)
+    def __str__(self):
+        return f"{self.name} {self.year}"
--- app/views.py
+++ app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import Movies
 # Create your views here.
 class HomePageView(TemplateView):
     template_name = 'home.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['movies'] = Movies.objects.all()[:30]
+        return context
