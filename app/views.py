3fd5d92ef451af6b4d4ac35543cb565c3a4b0daa
Александр Шаринский <alexandersh@tpu.ru>
2025-03-20 15:06:05 +0700

    Сортировка по названию, оценке и году и фильтрация по жанрам.

--- app/views.py
+++ app/views.py
@@ -6,5 +6,22 @@ class HomePageView(TemplateView):
     template_name = 'home.html'
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['movies'] = Movie.objects.all()[:100]
+        request = self.request
+        genre = request.GET.get('genre', 'All')
+        sort = request.GET.get('sort', 'year')
+        if genre != 'All':
+            movies = Movie.objects.filter(genre=genre)
+        else:
+            movies = Movie.objects.all()
+        if sort == 'title':
+            movies = movies.order_by('name', 'year')
+        elif sort == 'highest score':
+            movies = movies.filter(score__isnull=False).order_by('-score')
+        elif sort == 'lowest score':
+            movies = movies.filter(score__isnull=False).order_by('score')
+        elif sort == 'year':
+            movies = movies.order_by('year', 'name')
+        context['selected_genre'] = genre
+        context['selected_sort'] = sort
+        context['movies'] = movies[:100]
         return context
