e0994d55466eae8e9711517e36c641b3390a2597
Александр Шаринский <alexandersh@tpu.ru>
2025-11-02 22:50:13 +0700

    Добавление страницы с оценками пользователя, добавление глобальных переменных в main.css. Изменение списка жанров в search.html

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import SearchPageView, HomePageView, add_to_favorites, favorites, movie_detail, rate_movie, remove_from_favorites
+from .views import SearchPageView, HomePageView, add_to_favorites, favorites, ratings, movie_detail, rate_movie, remove_from_favorites
 
 urlpatterns = [
     path('search/', SearchPageView.as_view(), name='search'),
@@ -7,6 +7,7 @@ urlpatterns = [
     path('movie/<int:movie_id>/favorite/remove/', remove_from_favorites, name='remove_from_favorites'),
     path('movie/<int:movie_id>/', movie_detail, name='movie_detail'),
     path('favorites/<int:user_id>/', favorites, name='favorites'),
+    path('ratings/<int:user_id>/', ratings, name='ratings'),
     path('movie/<int:movie_id>/rate/', rate_movie, name='rate_movie'),
     path('', HomePageView.as_view(), name='home'),
 ]
