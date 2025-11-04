80d45ee5fea5433e6d1ebd1234d7e46dcdb2b39d
Александр <shsashao811@gmail.com>
2025-11-04 20:56:31 +0700

    Страница профиля пользователя

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import redirect, render, get_object_or_404
+
+from app.models import Favorite, MovieRating
 from .forms import CustomUserCreationForm
 from django.contrib.auth import logout
 from django.contrib.auth.models import User
@@ -23,4 +25,19 @@ def register(request):
 @login_required
 def profile(request, user_id):
     user = get_object_or_404(User, id=user_id)
-    return render(request, 'registration/profile.html', {'user': user})
+    favorites = Favorite.objects.filter(user=user).select_related('movie')
+    favorites_count = favorites.count()
+    ratings = MovieRating.objects.filter(user=user).select_related('movie')
+    ratings_count = ratings.count()
+    user_favorites_ids = Favorite.objects.filter(user=user).values_list('movie_id', flat=True)
+    
+    context = {
+        'user': user,
+        'favorites': favorites,
+        'ratings': ratings,
+        'favorites_count': favorites_count,
+        'ratings_count': ratings_count,
+        'user_favorites_ids': user_favorites_ids,
+    }
+    
+    return render(request, 'registration/profile.html', context)
