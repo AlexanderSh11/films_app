4e71f0e6da8b85ce17fb112a75be51e7c80d6d33
Александр Шаринский <alexandersh@tpu.ru>
2026-02-15 13:36:34 +0700

    Решение проблемы с API и постерами к фильмам

--- myapp/settings.py
+++ myapp/settings.py
@@ -137,3 +137,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
+
+MEDIA_URL = '/posters/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
